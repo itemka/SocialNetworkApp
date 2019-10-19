230e5954d8b64894138c810219f269eac7b6f8ac
itemka <itemka2503@gmail.com>
2019-10-19 21:26:22 +0300

    Updates

---
+++ precache-manifest.d5eefce485f3ffe33445f58ca17723e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5f12caf1e442654647da07aaabd3ef",
+    "url": "/SocialNetworkApp/index.html"
+  },
+  {
+    "revision": "86f1925b926b3e078f6f",
+    "url": "/SocialNetworkApp/static/css/main.a7ff0fba.chunk.css"
+  },
+  {
+    "revision": "36ae9b23ce734def7d15",
+    "url": "/SocialNetworkApp/static/js/2.469ecea5.chunk.js"
+  },
+  {
+    "revision": "86f1925b926b3e078f6f",
+    "url": "/SocialNetworkApp/static/js/main.6edea3f9.chunk.js"
+  },
+  {
+    "revision": "72010298f9fffa750bbf",
+    "url": "/SocialNetworkApp/static/js/runtime~main.966b10e4.js"
+  },
+  {
+    "revision": "0cfb3a5fd0f426bdba7f46a8722498ae",
+    "url": "/SocialNetworkApp/static/media/Preloader.0cfb3a5f.svg"
+  },
+  {
+    "revision": "a1652bca94248831188831774bb267ce",
+    "url": "/SocialNetworkApp/static/media/Spinner42px.a1652bca.svg"
+  },
+  {
+    "revision": "728ca8a3431f4a3428da1816e682d470",
+    "url": "/SocialNetworkApp/static/media/manBeard.728ca8a3.png"
+  }
+]);
